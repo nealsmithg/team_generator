4c92ac0f5698434a4aabac5a58c20bc2571e366c
Cornelius Smith <nealsmithg@gmail.com>
2021-10-12 13:16:08 -0400

    working on problem with inquirer not waiting

---
+++ index.js
@@ -0,0 +1,23 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const async = require("async")
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const { inherits } = require("util");
+
+function init() {
+    const manager = new Manager();
+    async.series({
+        function(callback){
+        manager.getName();
+        }
+    })
+    manager.getName();
+    manager.getId();
+    manager.getEmail();
+    manager.getOffice();
+    console.log(manager);
+};
+
+init();
