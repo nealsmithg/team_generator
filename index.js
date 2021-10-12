94d34169c0a8bb8a5359a0c91a36d6f05e882dfb
Cornelius Smith <nealsmithg@gmail.com>
2021-10-12 16:40:38 -0400

    fixed classes and started questions to create employees

--- index.js
+++ index.js
@@ -1,18 +1,91 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const async = require("async")
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const { inherits } = require("util");
+const Employee = require("./lib/Employee");
+const employees = []
+
+const startQuestions = [
+    {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the employee's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the employee's Email?",
+        name: "email"
+    }
+];
+
+const employeeType = [
+    {
+        type: "list",
+        message: "What type of Emplyee are you adding?",
+        name: "type",
+        choices: ["Engineer", "Intern", "No more"]
+    }
+]
+
+function createEmployee(){
+    inquirer
+    .prompt(employeeType)
+    .then((response) => {
+    if(response.type == "Engineer"){
+        createEngineer();
+    }else if(response.type == "Intern"){
+        createIntern();
+    }else{
+
+    }
+    })
+};
+
+function createEngineer(){
+    startQuestions.push({type: "input",
+    message: "What is the employee's Github username?",
+    name: "github"})
+
+    inquirer
+    .prompt(startQuestions)
+    .then((response) => {const engineer = new Engineer(response.name, response.id, response.email, response.gihub)
+        employees.push(engineer)
+        createEmployee();
+    })
+}
+
+function createIntern(){
+    startQuestions.push({type: "input",
+    message: "What is the employee's school?",
+    name: "school"})
+
+    inquirer
+    .prompt(startQuestions)
+    .then((response) => {const intern = new Intern(response.name, response.id, response.email, response.school)
+        employees.push(intern)
+        createEmployee();
+    })
+}
 
 function init() {
-    const manager = new Manager();
-    manager.getName();
-    manager.getId();
-    manager.getEmail();
-    manager.getOffice();
-    console.log(manager);
+    startQuestions.push({type: "input",
+    message: "What is the employee's office number?",
+    name: "office"})
+
+    inquirer
+    .prompt(startQuestions)
+    .then((response) => {const manager = new Manager(response.name, response.id, response.email, response.office)
+        employees.push(manager);
+        createEmployee();
+    })
 };
+
 
 init();
